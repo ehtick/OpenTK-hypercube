8344df21fa95bc8083de166ee51a3c9318f02883
Tornado Tech <54727692+TornadoTechnology@users.noreply.github.com>
2025-07-24 07:26:07 +1000

    feat: funny model system

---
+++ src/Hypercube.Core/Systems/Rendering/ModelComponent.cs
@@ -0,0 +1,26 @@
+﻿using Hypercube.Core.Ecs;
+using Hypercube.Core.Graphics.Resources;
+using Hypercube.Core.Resources;
+using Hypercube.Core.Serialization;
+using Hypercube.Mathematics;
+using Hypercube.Mathematics.Quaternions;
+using Hypercube.Mathematics.Vectors;
+
+namespace Hypercube.Core.Systems.Rendering;
+
+public class ModelComponent : Component
+{
+    public Model? Model;
+
+    [DataField("model")]
+    public ResourcePath Path = ResourcePath.Empty;
+
+    [DataField]
+    public Quaternion Rotation = Quaternion.FromEuler(Vector3.Zero);
+
+    [DataField]
+    public Vector3 Scale = Vector3.One;
+    
+    [DataField]
+    public Color Color = Color.White;
+}
---
+++ src/Hypercube.Core/Systems/Rendering/ModelSystem.cs
@@ -0,0 +1,56 @@
+﻿using Hypercube.Core.Ecs;
+using Hypercube.Core.Ecs.Attributes;
+using Hypercube.Core.Ecs.Core.Query;
+using Hypercube.Core.Ecs.Events;
+using Hypercube.Core.Graphics.Rendering.Context;
+using Hypercube.Core.Graphics.Rendering.Manager;
+using Hypercube.Core.Graphics.Resources;
+using Hypercube.Core.Resources;
+using Hypercube.Core.Systems.Transform;
+using Hypercube.Mathematics.Quaternions;
+using Hypercube.Mathematics.Vectors;
+using Hypercube.Utilities.Dependencies;
+
+namespace Hypercube.Core.Systems.Rendering;
+
+[RegisterEntitySystem]
+public sealed class ModelSystem : PatchEntitySystem
+{
+    [Dependency] private readonly IResourceManager _resource = default!;
+    
+    private EntityQuery _query = default!;
+    
+    public override void Startup()
+    {
+        base.Startup();
+
+        _query = EntityQueryBuilder
+            .With<TransformComponent>()
+            .With<ModelComponent>()
+            .Build();
+        
+        Subscribe<ModelComponent, AddedEvent>(OnAdded);
+    }
+
+    private void OnAdded(ref Entity entity, ref ModelComponent component, ref AddedEvent args)
+    {
+        component.Model = _resource.Get<Model>(component.Path);
+    }
+    
+    public override void Draw(IRenderContext renderer)
+    {
+        var enumerator = _query.GetEnumerator;
+        while (enumerator.MoveNext(out var entity))
+        {
+            var transformComponent = GetComponent<TransformComponent>(entity);
+            var modelComponent = GetComponent<ModelComponent>(entity);
+
+            var position = transformComponent.LocalPosition;
+  
+            if (modelComponent.Model is null)
+                continue;
+            
+            renderer.DrawModel(modelComponent.Model, position, modelComponent.Rotation, modelComponent.Scale, modelComponent.Color);
+        }
+    }
+}
